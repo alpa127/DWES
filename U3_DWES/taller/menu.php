c636d19b1a14deed02d06167e3b360b234c24be2
alpa127 <alvaropachon028@gmail.com>
2023-11-09 10:02:42 +0100

    commit

--- U3_DWES/taller/menu.php
+++ U3_DWES/taller/menu.php
@@ -1,75 +1,47 @@
 <?php
-  //Ver si hay usuario logueado
-  session_start();
-  if(isset($_SESSION['usuario'])){
-    $us = $_SESSION['usuario'];
-    // switch($us->getPerfil()){
-    //   case 'A':
-    //     pintarMenuAdmin();
-    //   case 'M':
-    //     pintarMenuMecanico();
-    // }
-  }
-  else{
-    //Redirigir a login
-    header('location:../usuario/login.php');
-  }
-
-   ?>
-   <!-- Grey with black text -->
+//Ver si hay usuario logueado
+session_start();
+$us = null;
+if (isset($_SESSION['usuario'])) {
+  $us = $_SESSION['usuario'];
+} else {
+  //Redirigir a login
+  header('location:../usuario/login.php');
+}
+?>
+<!-- Grey with black text -->
 <nav class="navbar navbar-expand-sm bg-light navbar-light">
   <div class="container-fluid">
     <ul class="navbar-nav">
       <?php
-      if($us->getPerfil()=='A'){
-      echo '<li class="nav-item">
-              <a class="nav-link active" href="#">Usuarios</a>
-            </li>';
+      if ($us->getPerfil() == 'A') {
+        echo '<li class="nav-item">
+                <a class="nav-link active" href="../usuario/controllerUsuario.php">Usuarios</a>
+              </li>';
       }
       ?>
       <li class="nav-item">
-        <a class="nav-link active" href="controllerPieza.php">Pieza</a>
+        <a class="nav-link active" href="../pieza/controllerPieza.php">Piezas</a>
       </li>
-
+      <li class="nav-item">
+        <a class="nav-link active" href="../cliente/controllerCliente.php">Clientes</a>
+      </li>
       <?php
-      if($us->getPerfil()=='M'){
-      echo '<li class="nav-item">
-              <a class="nav-link active" href="#">Reparaciones</a>
-            </li>';
+      if ($us->getPerfil() == 'M') {
+        echo '<li class="nav-item">
+                <a class="nav-link active" href="../reparacion/conotrollerReparacion.php">Reparaciones</a>
+              </li>';
       }
       ?>
+    </ul>
+    <ul class="navbar-nav ms-auto">
       <li class="nav-item">
-        <?php echo $us!=null ? $us->getNombre():""; ?>
+        <span class="nav-link"><?php echo $us->getNombre() ?></span>
+
+      </li>
+      <li class="nav-item">
         <a class="nav-link active" href="../usuario/login.php?accion=cerrar">Salir</a>
       </li>
-     
     </ul>
   </div>
 </nav>
-<?php
-  
-  
-
-?>
-<!-- Grey with black text -->
-<!-- <nav class="navbar navbar-expand-sm bg-light navbar-light">
-  <div class="container-fluid">
-    <ul class="navbar-nav">
-    <li class="nav-item">
-        <a class="nav-link active" href="login.php">Usuarios</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link active" href="controllerPieza.php">Pieza</a>
-      </li>
-     <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav> -->
