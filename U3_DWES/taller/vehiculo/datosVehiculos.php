23b26f7bb134d203cb35328f91a274b0c8e12c70
alpa127 <alvaropachon028@gmail.com>
2023-11-13 10:19:41 +0100

    commit

--- U3_DWES/taller/vehiculo/datosVehiculos.php
+++ U3_DWES/taller/vehiculo/datosVehiculos.php
@@ -28,21 +28,21 @@ function marcarOptionSeleccionado($option, $optionSeleccionado)
                     <?php
                     foreach ($vehiculos as $v) {
                         echo '<tr>';
-                        if (isset($_POST['modif']) and $_POST['modif'] == $u->getId()) {
+                        if (isset($_POST['modif']) and $_POST['modif'] == $v->getId()) {
                             //Pintar campos para poder modificar
-                            echo '<td> <input type="text" name="id" disabled="disabled" value="' . $u->getId() . '"/></td>';
-                            echo '<td> <input type="text" name="dni" value="' . $u->getDni() . '"/></td>';
-                            echo '<td> <input type="text" name="nombre" value="' . $u->getNombre() . '"/></td>';
+                            echo '<td> <input type="text" name="id" disabled="disabled" value="' . $v->getId() . '"/></td>';
+                            echo '<td> <input type="text" name="dni" value="' . $v->getDni() . '"/></td>';
+                            echo '<td> <input type="text" name="nombre" value="' . $v->getNombre() . '"/></td>';
                             echo '<td><select name="perfil">';
-                            echo '<option value="A" ' . marcarOptionSeleccionado('A', $u->getPerfil()) . '>Administrador</option>';
-                            echo '<option value="M" ' . marcarOptionSeleccionado('M', $u->getPerfil()) . '>Mecánico</option>';
+                            echo '<option value="A" ' . marcarOptionSeleccionado('A', $v->getPerfil()) . '>Administrador</option>';
+                            echo '<option value="M" ' . marcarOptionSeleccionado('M', $v->getPerfil()) . '>Mecánico</option>';
                             echo '</select></td>';
                             echo '<td>';
-                            echo '<button type="submit" class="btn btn-outline-dark" name="update" value="' . $u->getId() . '">Guardar</button>';
+                            echo '<button type="submit" class="btn btn-outline-dark" name="update" value="' . $v->getId() . '">Guardar</button>';
                             echo '<button type="submit" class="btn btn-outline-dark" name="cancelar">Cancelar</button>';
                             echo '</td>';
                         } else {
-                            switch ($u->getPerfil()) {
+                            switch ($v->getPerfil()) {
                                 case 'A':
                                     $perfil = 'Administrador';
                                     break;
@@ -53,13 +53,13 @@ function marcarOptionSeleccionado($option, $optionSeleccionado)
                                     $perfil = 'Error';
                                     break;
                             }
-                            echo '<td>' . $u->getId() . '</td>';
-                            echo '<td>' . $u->getDni() . '</td>';
-                            echo '<td>' . $u->getNombre() . '</td>';
+                            echo '<td>' . $v->getId() . '</td>';
+                            echo '<td>' . $v->getDni() . '</td>';
+                            echo '<td>' . $v->getNombre() . '</td>';
                             echo '<td>' . $perfil . '</td>';
                             echo '<td>';
-                            echo '<button type="submit" class="btn btn-outline-dark" name="modif" value="' . $u->getId() . '"><img src="../icon/modif25.png"/></button>';
-                            echo '<button type="button" class="btn btn-outline-dark"  data-bs-toggle="modal"  data-bs-target="#a' . $u->getId() . '" name="avisar" value="' . $u->getId() . '"><img src="../icon/delete25.png"/></button>';
+                            echo '<button type="submit" class="btn btn-outline-dark" name="modif" value="' . $v->getId() . '"><img src="../icon/modif25.png"/></button>';
+                            echo '<button type="button" class="btn btn-outline-dark"  data-bs-toggle="modal"  data-bs-target="#a' . $v->getId() . '" name="avisar" value="' . $v->getId() . '"><img src="../icon/delete25.png"/></button>';
                             echo '</td>';
                         }
                         echo '</tr>';
@@ -67,7 +67,7 @@ function marcarOptionSeleccionado($option, $optionSeleccionado)
                         //Definir ventana modal
                     ?>
                         <!-- The Modal -->
-                        <div class="modal" id="a<?php echo $u->getDni(), $u->getNombre(); ?>">
+                        <div class="modal" id="a<?php echo $v->getDni(), $v->getNombre(); ?>">
                             <div class="modal-dialog">
                                 <div class="modal-content">
 
@@ -84,7 +84,7 @@ function marcarOptionSeleccionado($option, $optionSeleccionado)
 
                                     <!-- Modal footer -->
                                     <div class="modal-footer">
-                                        <button type="submit" name="borrar" value="<?php echo $u->getId(); ?>" class="btn btn-danger" data-bs-dismiss="modal">Borrar</button>
+                                        <button type="submit" name="borrar" value="<?php echo $v->getId(); ?>" class="btn btn-danger" data-bs-dismiss="modal">Borrar</button>
                                     </div>
 
                                 </div>
