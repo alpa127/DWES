8bdb7935e56b7cb857abd2025b04a9f07c31e3b2
alpa127 <alvaropachon028@gmail.com>
2023-11-23 08:46:27 +0000

    commit

--- U3_DWES/taller/Modelo.php
+++ U3_DWES/taller/Modelo.php
@@ -22,6 +22,21 @@ class Modelo
             echo $e->getMessage();
         }
     }
+    function modificarReparacion(int $id,float $horas,bool $pagado,float $precioH){
+        try {
+            $consulta = $this->conexion->prepare('update reparacion ser tiempo=?, 
+            pagado=?,precioH=? where id = ?');
+            $params = array($horas,$pagado,$precioH,$id);
+            if($consulta->execute($params)){
+                if($consulta->rowCount()==1){
+                    return true;
+                }
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return false;
+    }
     function crearReparacion(Reparacion $r)
     {
         $resultado = false;
