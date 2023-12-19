4fd83d4d1f2453048cd95f478d87ecb65725b35a
alpa127 <alvaropachon028@gmail.com>
2023-12-19 09:54:58 +0000

    commit

--- U3_DWES/Examen2324/Modelo.php
+++ U3_DWES/Examen2324/Modelo.php
@@ -1,122 +1,136 @@
 <?php
- require_once 'mcDaw.php';
- require_once 'Tienda.php';
- require_once 'Producto.php';
+require_once 'Producto.php';
+require_once 'ProductoEnCesta.php';
+require_once 'Tienda.php';
+class Modelo{
+    private string $url = 'mysql:host=localhost;port=3306;dbname=mcDaw';
+    private string $us = 'root';
+    private string $ps = '';
 
- class Modelo{
-
-    private $conexion;
-
-    const URL = 'mysql:host=localhost;port=3306;dbname=mcdaw';
-    const USUARIO = 'root';
-    const PS = '';
+    private $conexion = null;
 
     function __construct()
     {
         try {
-            //Establecer conexión con la BD
-            $this->conexion = new PDO(Modelo::URL, Modelo::USUARIO, Modelo::PS);
+            $this->conexion = new PDO($this->url, $this->us, $this->ps);
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
 
-    function obtenerTienda()
-    {
-        $resultado = array();
-        try {
-            $consulta = $this->conexion->prepare('SELECT * from tienda 
-            order by nombre');
-            $params = array();
-            if ($consulta->execute($params)) {
-                if ($fila = $consulta->fetch()) {
-                    $resultado[] = new Tienda(
-                        $fila['codigo'],
-                        $fila['nombre'],
-                        $fila['telefono']
-                        
-                    );
+    //Prepare = Si en la consulta hay ?
+    //Query = Todas las demás
+
+    function obtenerTiendas(){
+        $resultado=array();
+        try{
+            $consulta = $this->conexion->query('SELECT * from tienda order by nombre');
+            if($consulta->execute()){
+                while($fila=$consulta->fetch()){
+                    $resultado[]=new Tienda($fila['codigo'],$fila['nombre'],$fila['telefono']);                   
                 }
             }
-        } catch (PDOException $e) {
+        }catch(PDOException $e){
             echo $e->getMessage();
         }
         return $resultado;
     }
 
-    function obtenerDatosTienda($codT)
-    {
-        $resultado = null;
-        try {
-            $consulta = $this->conexion->prepare('SELECT * from tienda 
-            where codigo = ?');
-            $params = array($codT);
-            if ($consulta->execute($params)) {
-                if ($fila = $consulta->fetch()) {
-                    $resultado = new Tienda(
-                        $fila['codigo'],
-                        $fila['nombre'],
-                        $fila['telefono']
-                        
-                    );
+    function obtenerTienda($codigo){
+        $resultado=null;
+        try{
+            $consulta = $this->conexion->prepare('SELECT * from tienda where codigo=?');
+            $params=array($codigo);
+            if($consulta->execute()){
+                if ($fila=$consulta->fetch()){
+                    $resultado=new Tienda($fila['codigo'],$fila['nombre'],$fila['telefono']);                   
                 }
             }
-        } catch (PDOException $e) {
+        }catch(PDOException $e){
             echo $e->getMessage();
         }
         return $resultado;
     }
 
-    function obtenerProducto()
-    {
-        $resultado = array();
-        try {
-            $consulta = $this->conexion->prepare('SELECT * from producto 
-            order by nombre');
-            $params = array();
-            if ($consulta->execute($params)) {
-                if ($fila = $consulta->fetch()) {
-                    $resultado = new Tienda(
-                        $fila['codigo'],
-                        $fila['nombre'],
-                        $fila['telefono']
-                        
-                    );
+    function obtenerProductos(){
+        $resultado=array();
+        try{
+            $consulta = $this->conexion->prepare('SELECT * from producto order by nombre');
+            if($consulta->execute()){
+                while($fila=$consulta->fetch()){
+                    $resultado[]=new Producto($fila['codigo'],$fila['nombre'],$fila['precio']);                   
                 }
             }
-        } catch (PDOException $e) {
+        }catch(PDOException $e){
             echo $e->getMessage();
         }
         return $resultado;
     }
 
-    function obtenerDatosProducto($codP)
-    {
-        $resultado = null;
-        try {
-            $consulta = $this->conexion->prepare('SELECT * from tienda 
-            where codigo = ?');
-            $params = array($codP);
-            if ($consulta->execute($params)) {
-                if ($fila = $consulta->fetch()) {
-                    $resultado = new Tienda(
-                        $fila['codigo'],
-                        $fila['nombre'],
-                        $fila['telefono']
-                        
-                    );
+    
+    function obtenerDatosTienda($tienda){
+        $resultado=null;
+        try{
+            $consulta = $this->conexion->prepare('SELECT * from tienda where codigo = ?');
+            $params=array($tienda);
+            if($consulta->execute($params)){
+                if($fila=$consulta->fetch()){
+                    $resultado=new Tienda($fila['codigo'],$fila['nombre'],$fila['telefono']);                   
                 }
             }
-        } catch (PDOException $e) {
+        }catch(PDOException $e){
             echo $e->getMessage();
         }
         return $resultado;
+    }
+
+
+    function obtenerSeleccionado($code){
+        $resultado=null;
+        try{
+            $consulta = $this->conexion->prepare('SELECT * from producto where codigo = ?');
+            $params=array($code);
+            if($consulta->execute($params)){
+                if($fila=$consulta->fetch()){
+                    $resultado=new Producto($fila['codigo'],$fila['nombre'],$fila['precio']);                   
+                }
+            }
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+
+    function crearPedido($tienda, $cesta)
+    {
+        try {
+            $this->conexion->beginTransaction();
+            //Crear Pedido
+            $consulta=$this->conexion->prepare('INSERT into pedido values(default, curdate(), ?');
+            $params=array($tienda);
+            if ($consulta->execute($params)) {
+                
+                //Inserts de productos en cesta
+                $linea=0;
+                foreach ($cesta as $pc) {
+                    $consulta=$this->conexion->prepare('INSERT into detalle values(?,?,?,?,?');
+                    
+                    $params=array($linea++,);
+
+
+                }
+            }
+        
+        } catch (PDOException $e) {
+            $this->conexion->rollBack();
+            echo $e->getMessage();
+        }
     }
 
 
     /**
      * Get the value of conexion
-     */
+     */ 
     public function getConexion()
     {
         return $this->conexion;
@@ -126,13 +140,12 @@
      * Set the value of conexion
      *
      * @return  self
-     */
+     */ 
     public function setConexion($conexion)
     {
         $this->conexion = $conexion;
 
         return $this;
     }
-
- }
+}    
 ?>
