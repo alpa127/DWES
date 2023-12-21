237047a56e02fe6e8af8c17241f026f01ab97d80
alpa127 <alvaropachon028@gmail.com>
2023-12-21 09:08:02 +0000

    commit

---
+++ U3_DWES/2324ExamenT3/Modelo.php
@@ -0,0 +1,162 @@
+<?php
+require_once 'Tienda.php';
+require_once 'Producto.php';
+require_once 'ProductoEnCesta.php';
+class Modelo
+{
+    private $url = "mysql:host=localhost;port=3306;dbname=mcDaw";
+    private $us = "root";
+    private $ps = "";
+    private $conexion = null;
+
+    function __construct()
+    {
+        try {
+            $this->conexion = new PDO($this->url, $this->us, $this->ps);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    function obtenerInfoPedido($codigoPedido)
+    {
+        $resultado = array();
+        try {
+            $consulta =
+                $this->conexion->prepare('call datosPedido(?)');
+            $params = array($codigoPedido);
+            if ($consulta->execute($params)) {
+                if ($fila = $consulta->fetch()) {
+                    $resultado[] = $fila['numProd'];
+                    $resultado[] = $fila['total'];
+                }
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+    function crearPedido($tienda, $cesta)
+    {
+        $resultado = 0;
+        try {
+            //Iniciar transacción
+            $this->conexion->beginTransaction();
+            //Crear pedidio
+            $consulta = $this->conexion->prepare(
+                'INSERT into pedido values(default,curdate(),?)'
+            );
+            $params = array($tienda->getCodigo());
+            if ($consulta->execute($params)) {
+                //REcuperar el id del pedido generado
+                $idP = $this->conexion->lastInsertId();
+                //Inserts de productos en cesta
+                $linea = 0;
+                foreach ($cesta as $pc) {
+                    $consulta = $this->conexion->prepare(
+                        'INSERT into detalle values(?,?,?,?,?)'
+                    );
+                    $params = array(
+                        ++$linea, $idP, $pc->getProducto()->getCodigo(),
+                        $pc->getCantidad(), $pc->getProducto()->getPrecio()
+                    );
+                    if (!$consulta->execute($params)) {
+                        $this->conexion->rollBack();
+                        return 0;
+                    }
+                }
+                $this->conexion->commit();
+                $resultado = $idP;
+            } else {
+                $this->conexion->rollBack();
+            }
+        } catch (PDOException $e) {
+            $this->conexion->rollBack();
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+    function obtenerProducto($codigo)
+    {
+        $resultado = null;
+        try {
+            $consulta = $this->conexion->prepare("select * from producto where codigo = ?");
+            $params = array($codigo);
+            if ($consulta->execute($params)) {
+                if ($fila = $consulta->fetch()) {
+                    $resultado = new Producto($fila['codigo'], $fila['nombre'], $fila['precio']);
+                }
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+    function obtenerProductos()
+    {
+        $resultado = array();
+        try {
+            $consulta = $this->conexion->query("select * from producto");
+            if ($consulta->execute()) {
+                while ($fila = $consulta->fetch()) {
+                    $resultado[] = new Producto($fila['codigo'], $fila['nombre'], $fila['precio']);
+                }
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+
+    function obtenerTienda($codigo)
+    {
+        $resultado = null;
+        try {
+            $consulta = $this->conexion->prepare("select * from tienda where codigo = ?");
+            $params = array($codigo);
+            if ($consulta->execute($params)) {
+                if ($fila = $consulta->fetch()) {
+                    $resultado = new Tienda($fila['codigo'], $fila['nombre'], $fila['telefono']);
+                }
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+    function obtenerTiendas()
+    {
+        $resultado = array();
+        try {
+            $consulta = $this->conexion->query("select * from tienda");
+            if ($consulta->execute()) {
+                while ($fila = $consulta->fetch()) {
+                    $tienda = new Tienda($fila['codigo'], $fila['nombre'], $fila['telefono']);
+                    $resultado[] = $tienda;
+                }
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return $resultado;
+    }
+
+    /**
+     * Get the value of conexion
+     */
+    public function getConexion()
+    {
+        return $this->conexion;
+    }
+
+    /**
+     * Set the value of conexion
+     *
+     * @return  self
+     */
+    public function setConexion($conexion)
+    {
+        $this->conexion = $conexion;
+
+        return $this;
+    }
+}
---
+++ U3_DWES/2324ExamenT3/mcDaw.php
@@ -0,0 +1,158 @@
+<?php
+require_once 'Modelo.php';
+
+$bd = new Modelo();
+if ($bd->getConexion() == null) {
+    $mensaje = "Error, no hay conexión con la bd";
+} else {
+    session_start();
+    if (isset($_POST['selTienda'])) {
+        //Almacenar tienda en sesión
+        $tienda = $bd->obtenerTienda($_POST['tienda']);
+        if ($tienda != null)
+            $_SESSION['tienda'] = $tienda;
+        else
+            $mensaje = 'Error, tienda seleccionada no existe';
+    } elseif (isset($_POST['cambiar'])) {
+        session_destroy();
+        header('location:mcDaw.php');
+    } elseif (isset($_POST['agregar'])) {
+        if ($_POST['cantidad'] <= 0) {
+            $mensaje = 'Error, cantidad debe ser mayor que 0';
+        } else {
+            //Obtener todos los datos del producto seleccionado
+            $producto = $bd->obtenerProducto($_POST['producto']);
+            if ($producto != null) {
+                //Añadir a cesta
+                $proCesta = new ProductoEnCesta($producto, $_POST['cantidad']);
+                if (!isset($_SESSION['cesta'])) {
+                    $_SESSION['cesta'] = array();
+                    
+                }
+                $_SESSION['cesta'][] = $proCesta;
+            } else {
+                $mensaje = 'Error, producto no existe';
+            }
+        }
+    } elseif (isset($_POST['quiCesta'])) {
+        if (isset($_SESSION['cesta']) and !empty($_SESSION['cesta'])){
+            unset($_SESSION['cesta'][$_POST['quiCesta']]);
+            $_SESSION['cesta']=array_values($_SESSION['cesta']);
+        }else{}
+        
+    }elseif (isset($_POST['crearPedido'])) {
+        if (isset($_SESSION['cesta']) and !empty($_SESSION['cesta'])) {
+            $codigoPedido = $bd->crearPedido($_SESSION['tienda'], $_SESSION['cesta']);
+            if ($codigoPedido > 0) {
+                $mensaje = 'Pedido Creado';
+                unset($_SESSION['cesta']);
+                $datos = $bd->obtenerInfoPedido($codigoPedido);
+                $mensaje = 'Pedidio nº ' . $codigoPedido . ' generado. El nº de productos es' .
+                    $datos[0] . ' y el importe total es ' . $datos[1];
+            } else {
+                $mensaje = 'Error al crear el pedido';
+            }
+        } else {
+            $mensaje = 'Error, cesta vacía';
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <h1 style='color:red;'><?php echo isset($mensaje) ? $mensaje : ""; ?></h1>
+    </div>
+    <form action="mcDaw.php" method="post">
+        <?php
+
+        if (!isset($_SESSION['tienda'])) {
+        ?>
+            <div>
+                <h1 style='color:blue;'>Tienda</h1>
+                <label for="tienda">Tienda</label><br />
+                <select name="tienda">
+                    <?php
+                    $tiendas = $bd->obtenerTiendas();
+                    foreach ($tiendas as $t) {
+                        echo '<option value="' . $t->getCodigo() . '">' . $t->getNombre() . '</option>';
+                    }
+                    ?>
+                </select>
+                <button type="submit" name="selTienda">Seleccionar tienda</button>
+            </div>
+        <?php
+        } else {
+        ?>
+            <div>
+                <h1 style='color:blue;'>Añade productos a la cesta</h1>
+                <?php
+                $t = $_SESSION['tienda'];
+                ?>
+                <h2 style='color:green;'>Datos Tienda:
+                    <?php echo $t->getNombre(), '-', $t->getTelefono(); ?>
+                    <button type="submit" name="cambiar">Cambiar Tienda</button>
+                </h2>
+                <table>
+                    <tr>
+                        <td><label for="producto">Producto</label><br /></td>
+                        <td><label for="cantidad">Cantidad</label><br /></td>
+                        <td>Añadir a la cesta</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select id="producto" name="producto">
+                                <?php
+                                $productos = $bd->obtenerProductos();
+                                foreach ($productos as $p) {
+                                    echo '<option value="' . $p->getCodigo() . '">' . $p->getNombre() .
+                                        '-' . $p->getPrecio() . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </td>
+                        <td><input id="cantidad" type="number" name="cantidad" value="1" /></td>
+                        <td><button type="submit" name="agregar">+</button></td>
+                    </tr>
+                </table>
+            </div>
+            <div>
+                <h1 style='color:blue;'>Contenido de la cesta</h1>
+                <table border="1" rules="all" width="25%">
+                    <tr>
+                        <td><b>Producto</b></td>
+                        <td><b>Cantidad</b></td>
+                        <td><b>Precio</b></td>
+                        <td><b>Quitar de la cesta</b></td>
+                    </tr>
+                    <?php
+                    if (isset($_SESSION['cesta'])) {
+                        foreach ($_SESSION['cesta'] as $i=>$pc) {
+                            echo '<tr>';
+                            echo '<td>', $pc->getProducto()->getNombre(), '</td>';
+                            echo '<td>', $pc->getCantidad(), '</td>';
+                            echo '<td>', $pc->getProducto()->getPrecio(), '</td>';
+                            echo '<td style="text-align:center"> <button type="submit" value="',$i,'" name="quiCesta"><b>-</b></button></td>';
+                            echo '</tr>';
+                        }
+                    }
+                    ?>
+
+                </table>
+                <button type="submit" name="crearPedido">Crear Pedido</button>
+            </div>
+        <?php
+        }
+        ?>
+    </form>
+</body>
+
+</html>
